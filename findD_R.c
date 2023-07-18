bb88b7f35b7b65695a4ad347d68271524fc60834
Sudo-Redu <139532549+Sudo-Redu@users.noreply.github.com>
2023-07-18 15:13:17 +0530

    Add files via upload

---
+++ findD_R.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main(){
+//Taking input
+int n;
+printf("Enter a Number: ");
+scanf("%d",&n);
+int x;
+printf("Enter a Number to Divide: ");
+scanf("%d",&x);
+printf("It Reminder is: %d And quotient is: %d",n%x , n/x);
+
+}
